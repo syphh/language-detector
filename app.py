b60fdaace72c26f30f260fba7d53e9304fdc6768
Syphax Ait oubelli <syphaxp@gmail.com>
2023-12-04 10:22:48 +0100

    fix bugs

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ import detectlanguage as dl
 
 dl.configuration.api_key = '21b0e2c7cf444787f680d96ff5d3ea8f'
 
-application = Flask(__name__)
+app = Flask(__name__)
+application = app
 
-@application.route('/')
+@app.route('/')
 def hello_world():
     return render_template('index.html')
 
-@application.route('/detect_language', methods=['POST'])
+@app.route('/detect_language', methods=['POST'])
 def detect_language():
     print(request.form)
     text = request.form['input_text']
@@ -29,4 +30,4 @@ def detect_language():
     return render_template('index.html', detected_language=result_name, icon_link=icon_link)
 
 if __name__ == '__main__':
-    application.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
